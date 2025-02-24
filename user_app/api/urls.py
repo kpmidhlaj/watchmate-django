1871f6a21f997793d70ab533f29f9616d2d4faeb
kpmidhlaj <midhlajkp@cookee.io>
2025-02-24 11:06:31 +0530

    Reverted to main token

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -7,11 +7,11 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     # path('register/', UserCreateView.as_view(), name='register'),
-    # path('login/', obtain_auth_token, name='login'),
-    # path('register/', registration_view, name='register'),
-    # path('logout/', logout_view, name='logout'),
-    # path('logout/', obtain_auth_token, name='logout'),
+    path('login/', obtain_auth_token, name='login'),
     path('register/', registration_view, name='register'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/', logout_view, name='logout'),
+    # path('logout/', obtain_auth_token, name='logout'),
+    # path('register/', registration_view, name='register'),
+    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
